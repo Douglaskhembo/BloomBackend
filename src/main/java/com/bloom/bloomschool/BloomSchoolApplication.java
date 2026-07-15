0bea676a8b08a8e57da94ed8077b61d0c4bad5bf
DouglasKhembo <khembo.douglas.juma@gmail.com>
2026-07-15 21:34:47 +0300

    initial

---
+++ src/main/java/com/bloom/bloomschool/BloomSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.bloom.bloomschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bloom/bloomschool/BloomSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloom.bloomschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
